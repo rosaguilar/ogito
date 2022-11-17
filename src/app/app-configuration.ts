aec4cbc80f5afb24ad62c8233f065438c7314a0e
Rosa Aguilar <r.m.aguilardearchila@utwente.nl>
2022-11-17 10:07:44 +0100

    Update app-configuration.ts

--- src/app/app-configuration.ts
+++ src/app/app-configuration.ts
@@ -1,4 +1,17 @@
-
+/***************************************************************************
+    app-configuration.ts - static parameters in OGITO
+    ---------------------
+    begin                : Oct 2020
+    copyright            : (C) 2020 by Rosa Aguilar
+    email                :  rosamaguilar at gmail dot com / r.aguilar at utwente dot nl
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 export class AppConfiguration{
   static hostname = 'https://ogito.itc.utwente.nl/';
