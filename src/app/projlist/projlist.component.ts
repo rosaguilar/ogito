0d275d47f351682c5dd3c9d7df1657af31174a6e
Rosa Aguilar <r.m.aguilardearchila@utwente.nl>
2022-11-17 10:20:25 +0100

    Update projlist.component.ts

--- src/app/projlist/projlist.component.ts
+++ src/app/projlist/projlist.component.ts
@@ -1,3 +1,19 @@
+
+/***************************************************************************
+    projlist.component.ts - displays a list of available projects in OGITO
+    ---------------------
+    begin                : Oct 2020
+    copyright            : (C) 2020 by Rosa Aguilar
+    email                :  rosamaguilar at gmail dot com / r.aguilar at utwente dot nl
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {AppConfiguration} from '../app-configuration';
 import {Observable, of as observableOf} from 'rxjs';
