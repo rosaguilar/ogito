f740ece496e455e086cdda6517f5448ca331938f
Rosa Aguilar <r.m.aguilardearchila@utwente.nl>
2022-11-17 09:46:27 +0100

    Update map.component.ts

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,3 +1,19 @@
+/***************************************************************************
+    map.component.ts - map visualization in OGITO
+    ---------------------
+    begin                : Oct 2020
+    copyright            : (C) 2022 by Rosa Aguilar
+    email                :  rosamaguilar at gmail dot com / r.aguilar at utwente dot nl
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
 import {AfterViewInit, Component, ElementRef, EventEmitter, Inject, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
 import {MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition} from '@angular/material/snack-bar';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
