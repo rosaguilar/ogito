edb99f42965c5e36fadb3fd507664846c0a648c8
Rosa Aguilar <r.m.aguilardearchila@utwente.nl>
2023-01-20 16:47:04 +0100

    Update toolbar.component.ts

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,3 +1,20 @@
+/***************************************************************************
+    toolbar.component.ts - displays the main toolbar in OGITO
+    ---------------------
+    begin                : Oct 2020
+    copyright            : (C) 2020 by Rosa Aguilar
+    email                :  rosamaguilar at gmail dot com / r.aguilar at utwente dot nl
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
+
 import {Component, Inject, OnInit} from '@angular/core';
 import {Subscription} from 'rxjs';
 import {OpenLayersService} from '../open-layers.service';
