8784fee955ded94b4af14f9b8b70ed6d4af34e2a
Rosa Aguilar <r.m.aguilardearchila@utwente.nl>
2023-01-20 14:59:42 +0100

    Update dialog-org-exposed.component.ts

--- src/app/dialog-org-exposed/dialog-org-exposed.component.ts
+++ src/app/dialog-org-exposed/dialog-org-exposed.component.ts
@@ -1,3 +1,18 @@
+/***************************************************************************
+    map.component.ts - Organization Explosed Dialog in OGITO
+    ---------------------
+    begin                : Oct 2020
+    copyright            : (C) 2020 by Rosa Aguilar
+    email                :  rosamaguilar at gmail dot com / r.aguilar at utwente dot nl
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 import {Component, Inject, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
