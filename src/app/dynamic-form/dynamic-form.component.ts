bc6928c46fac2065254d4807dd0c72384115cfcb
Rosa Aguilar <r.m.aguilardearchila@utwente.nl>
2023-01-20 15:04:33 +0100

    Update dynamic-form.component.ts

--- src/app/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-form/dynamic-form.component.ts
@@ -1,3 +1,20 @@
+
+/***************************************************************************
+    dynamic-form-question.ts - To generate dynamic queries in OGITO
+    ---------------------
+    begin                : Oct 2020
+    copyright            : (C) 2020 by Rosa Aguilar
+    email                :  rosamaguilar at gmail dot com / r.aguilar at utwente dot nl
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
 import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output, NgModule, ElementRef, ViewChild} from '@angular/core';
 import {Observable, of, of as observableOf, Subject, Subscription} from 'rxjs';
 import {QuestionService} from '../question-service.service';
