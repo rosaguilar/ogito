a050c955ea188a38ffc279a3c40b721caf550578
Rosa Aguilar <r.m.aguilardearchila@utwente.nl>
2023-01-20 15:03:38 +0100

    Update dynamic-form-question.component.ts

--- src/app/dynamic-form-question/dynamic-form-question.component.ts
+++ src/app/dynamic-form-question/dynamic-form-question.component.ts
@@ -1,3 +1,18 @@
+/***************************************************************************
+    
+dynamic-form-question.ts 
+    ---------------------
+    begin                : Oct 2020
+    copyright            : (C) 2020 by Rosa Aguilar
+    email                :  rosamaguilar at gmail dot com / r.aguilar at utwente dot nl
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 import { Component, OnInit } from '@angular/core';
 
 @Component({
