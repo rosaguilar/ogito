9064237755ff027ba5d8cda2a7807d026ec63149
Rosa Aguilar <r.m.aguilardearchila@utwente.nl>
2023-01-20 16:46:01 +0100

    Update symbol-list.component.ts

--- src/app/symbol-list/symbol-list.component.ts
+++ src/app/symbol-list/symbol-list.component.ts
@@ -1,3 +1,18 @@
+/***************************************************************************
+    symbollist.component.ts - displays a list of symbols for editing in OGITO
+    ---------------------
+    begin                : Oct 2020
+    copyright            : (C) 2020 by Rosa Aguilar
+    email                :  rosamaguilar at gmail dot com / r.aguilar at utwente dot nl
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 import {AfterViewInit, Component, Directive, ElementRef, Input, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import { Observable, Subscription,  of as observableOf } from 'rxjs';
 import {OpenLayersService} from '../open-layers.service';
