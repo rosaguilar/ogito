11d6a72f50832e722c775c7a6b1c6bd0c0c6586b
Rosa Aguilar <r.m.aguilardearchila@utwente.nl>
2023-01-20 15:05:15 +0100

    Update editing-toolbar.component.ts

--- src/app/editing-toolbar/editing-toolbar.component.ts
+++ src/app/editing-toolbar/editing-toolbar.component.ts
@@ -1,3 +1,19 @@
+
+/***************************************************************************
+      editing-toolbar.ts - Toolbar for editing in OGITO
+    ---------------------
+    begin                : Oct 2020
+    copyright            : (C) 2020 by Rosa Aguilar
+    email                :  rosamaguilar at gmail dot com / r.aguilar at utwente dot nl
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 import {Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {Observable, Subscription, of as observableOf} from 'rxjs';
 import {OpenLayersService} from '../open-layers.service';
